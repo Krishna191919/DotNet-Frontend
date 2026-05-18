77078ff4f35df1f6368d948af3a3133146670908
Krishna191919 <krishnachaudhary191919@gmail.com>
2026-05-18 21:21:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="heading" style={{ backgroundColor: "red" }}>
+      <h1 style={{ color: "yellow" }}>Product Management</h1>
+    </div>
+  );
+}
+
+export default App;
