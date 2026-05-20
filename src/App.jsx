ef22a866890f856f55680d5a464018bd233a37ad
Krishna191919 <krishnachaudhary191919@gmail.com>
2026-05-20 18:28:05 +0545

    React frontend setup with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <div className="heading" style={{ backgroundColor: "red" }}>
+    <div className="heading" style={{ backgroundColor: "grey" }}>
       <h1 style={{ color: "yellow" }}>Product Management</h1>
+      <p style={{ color: "purple", backgroundColor: "white" }}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit
+        perferendis veniam at dicta repellat omnis sapiente fugiat fuga
+        laudantium minus, pariatur distinctio id ratione odio earum incidunt
+        impedit tempora molestiae.
+      </p>
     </div>
   );
 }
